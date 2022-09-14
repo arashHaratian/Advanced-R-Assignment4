339cbc90bb131b752b413049bb3fb4ae8d91ab02
Arash <45104106+arashHaratian@users.noreply.github.com>
2022-09-14 17:42:36 +0200

    adding the class, putting function inside the class

--- R/linreg.R
+++ R/linreg.R
@@ -6,7 +6,7 @@
 #' @param data is a `data.frame` object containing the dataset that is being used
 #'
 #' @return an `RC` class object containing the following items:
-#'\enumerate{
+#' \enumerate{
 #'  \item Regressions Coefficients
 #'  \item Fitted Values
 #'  \item Residuals
@@ -15,11 +15,11 @@
 #'  \item Variance of the Regression Coefficients
 #'  \item t-values
 #'  \item p-values
-#'}
+#' }
 #' @export
 #'
 #' @examples
-#'
+
 linreg <- function(formula,data){
   X <- model.matrix(formula,data)
   dependent <- all.vars(formula)[1]
@@ -36,15 +36,32 @@ linreg <- function(formula,data){
   t_values <- beta / sqrt(var_reg)
 
   p_values <- pt(-abs(t_values),df=df)
-  return(list(
-    beta,
-    as.vector(fitted_val),
-    as.vector(residual_val),
-    df,
-    res_var,
-    var_reg,
-    t_values,
-    p_values
-  ))
+
+  beta_vector <- as.vector(beta)
+  names(beta_vector) <- rownames(beta)
+
+  formula_string <- as.character(formula)
+  call <- paste(
+    "linreg(formula =",
+    formula_string[[2]],
+    "~",
+    formula_string[[3]],
+    ", data = ",
+    deparse(substitute(data)),
+    ")"
+  )
+
+  result <- linreg_RC$new(
+    "call" = match.call(),
+    "beta" = beta_vector,
+    "fitted_val" = as.vector(fitted_val),
+    "residual_val" = as.vector(residual_val),
+    "df" = df,
+    "res_var" = res_var,
+    "var_reg" = var_reg,
+    "t_values" = t_values,
+    "p_values" = p_values
+  )
+  return(result)
 }
 
